a5d0df02860f6ac87021abcbaafd8af725d7e6ad
diepnnguyen <diepnnguyen@kms-technology.com>
2026-01-12 11:42:52 +0700

    init commit

---
+++ src/main/java/com/diepnn/cryptotradingsimulation/CryptoTradingSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.diepnn.cryptotradingsimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTradingSimulationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoTradingSimulationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diepnn/cryptotradingsimulation/CryptoTradingSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diepnn.cryptotradingsimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoTradingSimulationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
